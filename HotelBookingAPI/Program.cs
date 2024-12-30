3a3d5a42ebda2640c3f87976b5877d427811ddee
Joshua Genato <joshgenato@gmail.com>
2024-12-30 13:16:40 +0800

    ASP.NET Core Web API Project Setup for Hotel Booking

--- HotelBookingAPI/Program.cs
+++ HotelBookingAPI/Program.cs
@@ -1,11 +1,25 @@
+using HotelBookingAPI.Connection;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+});
+
+// Initialize Serilog from appsettings.json
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .ReadFrom.Services(services));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<SqlConnectionFactory>();
 
 var app = builder.Build();
 
